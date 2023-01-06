94f725c9d787476a86665f2f273231c13eb46136
Konrad <joreu.konrad@gmail.com>
2023-01-05 21:37:09 -0300

    more wb course

--- Webscraping_course/Chapter1.py
+++ Webscraping_course/Chapter1.py
@@ -1,6 +1,25 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen('https://www.incepa.com.br/serie/arbol/produto/lm-arbol-canela-ac-20x120-r-7mm')
-bs = BeautifulSoup(html.read(), 'html.parser')
-print(bs.body)
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(), "lxml")
+        title = bsObj.body.h2
+    except AttributeError as e:
+        return None
+    return title
+
+
+title = getTitle("https://www.biancogres.com.br/produto/botanic")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
+    
+    
