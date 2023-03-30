da5e78be70525077779229baa2ecb3bf38253ce8
Joreu Konrad <joreu.konrad@portobello.com.br>
2023-03-30 20:17:46 -0300

    up

---
+++ Python Stuffs/IDK/name.py
@@ -0,0 +1,9 @@
+# __name__ is a special variable we use when interacting with modules
+
+#-It allows to make a distintion between:
+#   The modules we import/packages (ex:pandas)
+#   And the modules we do not import
+
+if __name__ == '__main__':
+    print(__name__)
+
