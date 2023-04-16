944592a6cb3bc6edc2b6021eda670f6e80060480
Joreu Konrad <joreu.konrad@portobello.com.br>
2023-04-16 02:54:51 -0300

    up

--- tests/TK/Functions/treeview addremove.py
+++ tests/TK/Functions/treeview addremove.py
@@ -31,32 +31,48 @@ data = [
     ['Maik',3,'Qualquer coisa'],
     ['Peido',4,'Qualquer coisa']
 ]
-'''
+
 count = 0
 for record in data:
     my_tree.insert(parent='',index='end',text='',iid=count,values=(record[0],record[1],record[2]))
     count += 1
-'''
 
+'''
 my_tree.insert(parent='',index='end',iid=0,text='',values=('John','1','Peperroni'))
 my_tree.insert(parent='',index='end',iid=1,text='',values=('Joreu','2','Calabreza'))
 my_tree.insert(parent='',index='end',iid=2,text='',values=('Catinga','3','N lembro'))
 my_tree.insert(parent='',index='end',iid=3,text='',values=('Caetano','4','Qualquer coisa'))
 my_tree.insert(parent='1',index='end',iid=4,text='',values=('Maik','2.1','Qualquer coisa'))
 my_tree.insert(parent='4',index='end',iid=5,text='',values=('Peido','2.2','Qualquer coisa'))
-my_tree.insert(parent='5',index='end',iid=6,text='',values=('Peidooooo','2.3','Qualquer coisa'))
-
-my_tree.insert(parent='',index='end',iid=8,text='',values=('Caetano','4','Qualquer coisa'))
-my_tree.insert(parent='1',index='end',iid=9,text='',values=('Maik','2.1','Qualquer coisa'))
-my_tree.insert(parent='4',index='end',iid=10,text='',values=('Peido','2.2','Qualquer coisa'))
-my_tree.insert(parent='5',index='end',iid=11,text='',values=('Peidooooo','2.3','Qualquer coisa'))
-my_tree.insert(parent='2',index='end',iid=12,text='',values=('Caetano','4','Qualquer coisa'))
-my_tree.insert(parent='1',index='end',iid=13,text='',values=('Maik','2.1','Qualquer coisa'))
-my_tree.insert(parent='4',index='end',iid=14,text='',values=('Peido','2.2','Qualquer coisa'))
-my_tree.insert(parent='5',index='end',iid=15,text='',values=('Peidooooo','2.3','Qualquer coisa'))
-
-
+'''
 my_tree.pack(pady=20)
+
+add_frame = Frame(root)
+add_frame.pack(pady=20)
+
+
+
+nl = Label(add_frame,text='Name')
+nl.grid(row=0,column=0)
+
+il = Label(add_frame,text='ID')
+il.grid(row=0,column=1)
+
+tl = Label(add_frame,text='Topping')
+tl.grid(row=0,column=2)
+
+
+# Entry boxes
+name_box = Entry(add_frame)
+name_box.grid(row=1,column=0)
+
+id_box = Entry(add_frame)
+id_box.grid(row=1,column=1)
+
+topping_box = Entry(add_frame)
+topping_box.grid(row=1,column=2)
+
+#print(my_tree.selection()[0])
 
 
 root.mainloop()
--- tests/TK/Projects/VisualObj.py
+++ tests/TK/Projects/VisualObj.py
@@ -7,9 +7,8 @@ from PIL import Image
 
 root = Tk()
 root.title("Teste App")
-root.geometry("1000x600")
+root.geometry("750x600")
 
-# Nome colunas: Cod.	Nome	Tipo	Tamanho
 
 # Frame 1
 frame1 = LabelFrame(root, text="Imagem 1", bg="green",fg="white", padx=100, pady=100)
@@ -29,10 +28,38 @@ frame3.grid(row=0, column=2)
 label_inframe3 = Label(frame3,text='TESTE3')
 label_inframe3.pack()
 
-frame_table = LabelFrame(root,text='Tabela',padx=100,pady=100)
-frame_table.grid(row=1, column=0)
-label_inframe2 = Label(frame_table,text='TESTE FRAME')
-label_inframe2.pack()
 
+my_tree = ttk.Treeview(root)
+my_tree['columns'] = ('Marca','Nome','Formato','Borda','Superficie')
+
+# Formating
+my_tree.column('#0',width=0, minwidth=0)
+my_tree.column('Marca',anchor=W,width=20)
+my_tree.column('Nome',anchor=W,width=150)
+my_tree.column('Formato',anchor=W,width=20)
+my_tree.column('Borda',anchor=W,width=80)
+my_tree.column('Superficie',anchor=W,width=80)
+
+# Create headings
+my_tree.heading('#0',text='',anchor=W)
+my_tree.heading('Marca',text='Marca',anchor=W)
+my_tree.heading('Nome', text='Nome',anchor=CENTER)
+my_tree.heading('Formato',text='Formato',anchor=W)
+my_tree.heading('Borda', text='Borda',anchor=CENTER)
+my_tree.heading('Superficie',text='Superficie',anchor=W)
+
+
+data = pd.read_excel('Table.xlsx',index_col=0)
+
+
+
+
+for record in range(len(data)):
+    my_tree.insert(parent='',index='end',text='',iid=record,values=(data.loc[record,'Marca'],data.loc[record,'Nome'],data.loc[record,'Formato'],data.loc[record,'Borda'],data.loc[record,'Superficie']))
+
+ 
+
+
+my_tree.grid(row=1,column=0,columnspan=3,padx=10,pady=10,ipadx=100)
 
 root.mainloop()
