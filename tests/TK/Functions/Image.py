a682a5fc2407fc69566035a9d343b1fb9452bf72
Joreu Konrad <joreu.konrad@portobello.com.br>
2023-04-16 23:17:21 -0300

    up

--- tests/TK/Functions/Image.py
+++ tests/TK/Functions/Image.py
@@ -4,7 +4,7 @@ from PIL import ImageTk,Image
 root = Tk()
 root.title('Images')
 
-my_img = ImageTk.PhotoImage(Image.open('Image.jpg'))
+my_img = ImageTk.PhotoImage(Image.open('G:/Meu Drive/Pastas Compartilhadas/Catalogo Concorrencia/Eliane/0- Eliane Imagens/0 - Versao antiga/Manila Marfim AC.jpg'))
 my_label = Label(image=my_img)
 my_label.pack()
 
