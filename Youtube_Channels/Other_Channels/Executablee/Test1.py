2b1620d58d60efbdd3c8f1dd3a9115127428e295
Konrad <joreu.konrad@gmail.com>
2023-09-21 21:17:48 -0300

    up

---
+++ Youtube_Channels/Other_Channels/Executablee/Test1.py
@@ -0,0 +1,10 @@
+import Test2
+import time
+
+Test2.func_teste2()
+
+print('teste1!')
+
+Test2.func_teste2()
+
+time.sleep(2000)
---
+++ Youtube_Channels/Other_Channels/Executablee/Test2.py
@@ -0,0 +1,5 @@
+
+def func_teste2():
+    return print('teste2 feito!')
+
+print('aaaaaa naooo')
